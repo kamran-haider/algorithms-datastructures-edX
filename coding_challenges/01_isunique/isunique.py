573f10b61982300e0519347f5a86a9ddc0f2ccee
kamran haider <kamranhaider.mb@gmail.com>
2018-04-05 15:34:16 -0400

    challenege 1.1 from ctci

---
+++ coding_challenges/01_isunique/isunique.py
@@ -0,0 +1,36 @@
+"""
+Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures?
+"""
+
+
+def is_unique(input_string):
+    """
+    Checks if a string all all unique characters, assuming the string contains ASCII characters.
+
+    Parameters
+    ----------
+    input_string : str
+        Input string consisting of English alphabets
+
+    Returns
+    -------
+    isunique : bool
+        True if string contains unique characters, otherwise False
+    """
+    isunique = True
+
+    char_counts = [0 for i in range(128)]
+    for c in input_string:
+        index = ord(c)
+        if char_counts[index] == 0:
+            char_counts[index] += 1
+        else:
+            isunique = False
+            break
+
+    return isunique
+
+
+if __name__ == "__main__":
+    print(is_unique("abcd"))
---
+++ coding_challenges/01_isunique/test_isunique.py
@@ -0,0 +1,13 @@
+"""
+Test script for isunique.py
+"""
+
+from isunique import is_unique
+
+def test_nonunique_string():
+    assert not is_unique("kamran")
+
+def test_unique_string():
+    assert is_unique("pytorch")
+
+    
